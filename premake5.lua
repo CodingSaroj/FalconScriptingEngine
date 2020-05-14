7a19c552658c62dd0e7e90c529ed6db61befdd41
SarojKumar10 <sarojkr699681@gmail.com>
2020-05-14 14:49:09 +0530

    Seperated test from main library's premake5.lua.

--- premake5.lua
+++ premake5.lua
@@ -37,42 +37,4 @@ workspace "Falcon"
             architecture "x86_64"
             system "linux"
 
-    project "Test"
-        kind "ConsoleApp"
-        language "C++"
-        targetdir "bin/%{cfg.buildcfg}/%{cfg.system}/%{cfg.architecture}"
-
-        includedirs
-        {
-            "src/"
-        }
-
-        libdirs
-        {
-            "lib/%{cfg.buildcfg}"
-        }
-
-        files
-        {
-            "test/main.cpp"
-        }
-
-        links
-        {
-            "Falcon"
-        }
-
-        filter "configurations:Debug"
-            symbols "On"
-            optimize "Off"
-
-        filter "configurations:Release"
-            optimize "Full"
-
-        filter "platforms:Linux32"
-            architecture "x86"
-            system "linux"
-
-        filter "platforms:Linux64"
-            architecture "x86_64"
-            system "linux"
+    
