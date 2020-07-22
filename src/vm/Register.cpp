e7118e941d381d5ceecae50c81251fe929c0c83c
SarojKumar10 <sarojkr699681@gmail.com>
2020-07-22 11:30:05 +0530

    Added OpCode.cpp and Register.cpp to git.

---
+++ src/vm/Register.cpp
@@ -0,0 +1,18 @@
+#include "Register.hpp"
+
+namespace Falcon
+{
+    namespace RegisterType
+    {
+        std::vector<std::string> names =
+        {
+            "A8", "A16", "A32", "A64",
+            "B8", "B16", "B32", "B64",
+            "C8", "C16", "C32", "C64",
+            "D8", "D16", "D32", "D64",
+            "R1", "R2",  "R3",  "R4",
+            "SP", "CMT", "AO0", "AO1",
+        };
+ 
+    }
+}
